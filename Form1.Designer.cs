7144eb99e48fc9d4564734b16f7c6567403c29e9
Yunus Emre Gül <yunemregul@gmail.com>
2020-11-10 12:02:08 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿using System.Drawing;
+
+namespace fSwitch
+{
+    partial class fLagSwitch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose the path to your program that will be lagged:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(93, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter your lag toggler key:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(294, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnTogglerShortcutSpecified);
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.OnTogglerShortcutKeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.ForeColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(54, 111);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(157, 13);
+            this.statusLabel.TabIndex = 6;
+            this.statusLabel.Text = "Waiting for settings to be filled...";
+            // 
+            // fLagSwitch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 136);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "fLagSwitch";
+            this.Text = "fLagSwitch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace fSwitch
+{
+    public partial class fLagSwitch : Form
+    {
+        private OpenFileDialog fileDialog;
+        private bool fileSpecified = false;
+        private bool keySpecified = false;
+
+        public fLagSwitch()
+        {
+            InitializeComponent();
+            fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Choose the path to your program that will be lagged:";
+            fileDialog.RestoreDirectory = true;
+            fileDialog.DefaultExt = "exe";
+            fileDialog.CheckPathExists = true;
+            fileDialog.CheckFileExists = true;
+            fileDialog.Multiselect = false;
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fileDialog.FileName;
+                fileSpecified = true;
+                statusTextUpdater();
+            }
+            else
+            {
+                fileSpecified = false;
+                statusTextUpdater();
+            }
+        }
+
+        private void OnTogglerShortcutKeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void OnTogglerShortcutSpecified(object sender, KeyEventArgs e)
+        {
+            textBox2.Text = e.KeyCode.ToString();
+            keySpecified = true;
+            statusTextUpdater();
+        }
+
+        private void statusTextUpdater()
+        {
+            if (keySpecified && fileSpecified)
+            {
+                statusLabel.ForeColor = Color.Green;
+                statusLabel.Text = "Everything is good! Waiting for lag key press...";
+            }
+            else
+            {
+                statusLabel.ForeColor = Color.Red;
+                statusLabel.Text = "Waiting for settings to be filled...";
+            }
+        }
+    }
+}
